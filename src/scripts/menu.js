da07e483f7e3769f96e96bf77b85918cc4821c54
Allan Reyes <chef@ulampinoy.com>
2024-01-16 10:50:24 +0100

    Changed anchor colors

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,3 +1,3 @@
 document.querySelector('.hamburger').addEventListener('click', () => {
-    document.querySelector('.nav-links').classList.toggle('expanded');
+    document.querySelector('.navLinks').classList.toggle('expanded');
 });
